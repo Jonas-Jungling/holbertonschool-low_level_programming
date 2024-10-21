2d8b2b6cdcf8f2d6271f94c1c5d458d813d89ecc
Jonas-Jungling <jonas.jungling@oclock.school>
2024-10-21 11:59:40 +0200

    puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - C programm
+ * Description:  programm that prints a string
+ * Return: value 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
