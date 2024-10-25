a47644fe791560f26b1e75e737ad5d8edce77a07
Jonas-Jungling <jonas.jungling@oclock.school>
2024-10-25 10:07:32 +0200

    n to 98

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,23 +10,4 @@ void times_table(void)
 	int table;
 	int result;
 
-	for (table = 0; table < 10 ; table++)
-	{
-		for (result = 0; result < 10 ; result *= table)
-		{
-			if (result < 10)
-			{
-				_putchar(result + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			else if (result >= 10)
-			{
-				_putchar((result / 10) + '0');
-				_putchar((result % 10) + '0');
-				_putchar(',');
-			}	_putchar(' ');
-			_putchar('\n');
-		}
-	}
 }
