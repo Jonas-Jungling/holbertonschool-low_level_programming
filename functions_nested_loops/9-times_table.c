1edcd148d899f4fb616339b6e1f22702b1ea3c73
Jonas-Jungling <jonas.jungling@oclock.school>
2024-10-25 10:48:26 +0200

    9 times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,13 +1,39 @@
 #include "main.h"
 /**
-* times_table - Entry point
-* description: Print the 9 times table, starting with 0
-* Return: always 0
-*/
+ * times_table - The 9 times table
+ *
+ * Description: Print the 9 times table starting with 0.
+ *
+ */
+
 
 void times_table(void)
 {
-	int table;
-	int result;
+	int i, j, result;
 
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			result = i * j;
+
+			if (j == 0)
+			{
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (result < 10)
+				{
+					_putchar(' ');
+				}
+				_putchar(result / 10 + '0');
+			}
+			_putchar((result % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
