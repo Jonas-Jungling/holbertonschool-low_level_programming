c02489c22ed5692a96a17dece7ecec8a8dfa2075
Jonas-Jungling <jonas.jungling@oclock.school>
2024-10-28 11:25:40 +0100

    print lines

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -10,16 +10,16 @@ void print_line(int n)
 {
 	int line;
 
-	for (line = 0 ; line <= n ; line++)
+	if (n <= 0)
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-		else
+		_putchar('\n');
+	}
+	else
+	{
+		for (line = 0; line < n; line++)
 		{
 			_putchar('_');
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
