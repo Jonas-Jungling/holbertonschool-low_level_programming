3db43c0e789ae60beff38cca078414cb110f81b2
Jonas-Jungling <jonas.jungling@oclock.school>
2024-10-28 13:54:25 +0100

    print diag

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+* print_diagonal - function that draw
+* Description: draw a diagonal line
+* @n: line to draw
+* Return: 0
+*/
+
+void print_diagonal(int n)
+{
+	int diag;
+	int space;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (diag = 0; diag < n; diag++)
+		{
+			for (space = 0 ; space < diag ; space++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
