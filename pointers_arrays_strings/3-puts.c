c4ddadf181cbff6c6e2b2942ae9a36b56cf72e1d
Jonas-Jungling <jonas.jungling@oclock.school>
2024-10-29 10:52:38 +0100

    puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _puts - function that print a string
+* description: print a string followed by a new line to stdout
+* @str: string to print
+*/
+
+void _puts(char *str)
+{
+	for (; *str != '\0' ; str++)
+	{
+		printf("%c", *str);
+	}
+		printf("\n");
+}
