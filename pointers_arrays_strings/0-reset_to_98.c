8c8c618fcb14323d57c3183f4cf15b19ef7ad5f1
Jonas-Jungling <jonas.jungling@oclock.school>
2024-10-29 09:24:53 +0100

    reset to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* reset_to_98 - take a pointer
+* description: take pointer to an int as parameter and updates the value to 98
+* @n: value to update
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef MYHEADER
+#define MYHEADER
+
+void reset_to_98(int *n);
+
+#endif
