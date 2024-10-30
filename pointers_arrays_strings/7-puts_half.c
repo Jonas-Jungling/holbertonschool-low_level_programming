48f9537c718d5cbea5262d49b8f47a4b845d1818
Jonas-Jungling <jonas.jungling@oclock.school>
2024-10-30 09:53:16 +0100

    half put

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -5,3 +5,29 @@
 * @str: string to print
 */
 
+void puts_half(char *str)
+{
+	int length = 0;
+	int half;
+	int n;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	if (length % 2 == 0)
+	{
+		half = length / 2;
+	}
+	else
+	{
+		half = (length + 1) / 2;
+	}
+
+	for (n = half ; str[n] != '\0' ; n++)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}
