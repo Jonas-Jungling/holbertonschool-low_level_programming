1769bda3d90b961225aabd4c62bf1fa054da7f75
Jonas-Jungling <jonas.jungling@oclock.school>
2024-11-06 10:08:54 +0100

    diagsum

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,1 +1,22 @@
 #include "main.h"
+#include <stdio.h>
+/**
+* print_diagsums - print sum
+* description: print the sum of the two diagonals of integers
+* @a: Pointer to the first element
+* @size: The size of the square matrix
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum_main = 0;
+	int sum_secondary = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum_main += a[i * (size + 1)];
+		sum_secondary += a[(i + 1) * (size - 1)];
+	}
+	printf("%d, %d\n", sum_main, sum_secondary);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MYHEADER
 #define MYHEADER
 
+void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
