e762e7fab67abe8c17b9bb3f4933f2b0069b2ace
Jonas-Jungling <jonas.jungling@oclock.school>
2024-11-13 21:46:33 +0100

    Refactor code to rename 4-main.c to 1-main.c and update code accordingly

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *malloc_checked - return a pointer
+ * Description: Return a pointer to the allocated memory
+ * @b: size of memory
+ * Return: pointer
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *mem;
+
+	mem = malloc(b);
+
+	if (mem == NULL)
+	{
+		exit(98);
+	}
+	return (mem);
+}
