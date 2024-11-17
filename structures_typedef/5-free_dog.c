8f2c7f48347de047ddaa66e272a1ab14a35c4426
Jonas-Jungling <jonas.jungling@oclock.school>
2024-11-17 21:59:47 +0100

    Add function free_dogs

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,3 +1,5 @@
+#include "dog.h"
+#include <stdlib.h>
 /**
  * free_dog - Frees memory allocated for a dog structure.
  * @d: Pointer to the dog structure to be freed.
@@ -7,5 +9,10 @@
  */
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+	return;
+
+	free(d->name);
+	free(d->owner);
 	free(d);
 }
