8770c9191ad16e8532a9e6044f88483a5f02824a
Jonas-Jungling <jonas.jungling@oclock.school>
2024-11-20 11:32:19 +0100

    Refactor function_pointers/3-op_functions.c and function_pointers/3-get_op_func.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,31 +1,40 @@
-#include <stdio.h>
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
 * main - check the code
+* @argc: The number of command-line arguments
+* @argv: An array of command-line arguments
 *
 * Return: Always 0.
 */
-int main(void)
+int main(int argc, char **argv)
 {
 	int a;
 	int b;
+	int result;
+	int (*ptr_op)(int, int);
 
 	if (argc != 4)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(98);
 	}
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
-	int (*ptr_op)(int, int) = get_op_func(argv[2]);
+	ptr_op = get_op_func(argv[2]);
 
 	if (ptr_op == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
+
+	result = ptr_op(a, b);
+	printf("%d\n", result);
+	return (0);
 
 }
