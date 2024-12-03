451711d1683d71c779fcc6aeec22921a4efdd67c
Jonas-Jungling <jonas.jungling@oclock.school>
2024-12-03 12:32:22 +0100

    Refactor lists.h and add free_list function

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,17 +9,16 @@
 */
 size_t print_list(const list_t *h)
 {
-	const list_t *current = h;
 	int length = 0;
 
-	while (current != NULL)
+	while (h != NULL)
 	{
-		if (current->str != NULL)
-			printf("%s\n", current->str);
-		else
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 
-		current = current->next;
+		h = h->next;
 		length++;
 	}
 
---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list.
+ * @head: Pointer to the head of the list.
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+	free(head);
+}
